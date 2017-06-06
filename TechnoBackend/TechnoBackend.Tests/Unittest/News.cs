42fba8fd8ccd1a13ff06171ecdba5194f3973816
Rombouts, wouter (1267841) <1267841rombouts@zuyd.nl>
2017-06-06 18:39:40 +0200

    Prepwork UsecaseNews

---
+++ TechnoBackend/TechnoBackend.Tests/Unittest/News.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechnoBackend.Tests.Unittest
+{
+    [TestClass]
+    public class News
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TechnoBackend/TechnoBackend/Business Logic/News/JsonNews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace TechnoBackend.Business_Logic.News
+{
+    public class JsonNews
+    {
+        [JsonProperty(PropertyName = "ArticleName")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "ArticleBody")]
+        public string Password { get; set; }
+    }
+}
