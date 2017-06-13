8a64be0d9c161495a69336377bfbd0e9de5a7b9c
TLA <TLAsm@DESKTOP-QV3JA3R>
2017-06-13 13:10:17 +0200

    First commit for initialisering. WE gaan ervoorr !!!

---
+++ TechnoBackend/TechnoBackend/Business Logic/Werkoverzicht/WerkoverzichtStart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http.Controllers;
+using TechnoBackend.DatabaseModel;
+
+namespace TechnoBackend.Business_Logic.Werkoverzicht
+{
+    public class WerkoverzichtStart
+    {
+        public static string WorkItemsFetchen()
+        {
+            string Test = "Testen";
+            return Test;
+       
+        }
+    }
+}
---
+++ TechnoBackend/TechnoBackend/Controllers/WerkoverzichtTonenController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TechnoBackend.Login;
+using TechnoBackend.Business_Logic.Werkoverzicht;
+
+
+namespace TechnoBackend.Controllers
+{
+    public class WerkoverzichtTonenController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 1)
+            {
+                var message = WerkoverzichtStart.WorkItemsFetchen();
+                Request.Headers.Add("Token", newtoken.Item1);
+                var response = Request.CreateResponse(message);
+                return response;
+            }
+            return Request.CreateResponse("Invalid action");
+        }
+    }
+}
