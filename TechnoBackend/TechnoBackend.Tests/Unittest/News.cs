0cc8d7bf68f70bb9549caef536ef177218a841bf
wouter rombouts <1267841rombouts@zuyd.nl>
2017-06-13 13:52:03 +0200

    Add NewsCreateTest

--- TechnoBackend/TechnoBackend.Tests/Unittest/News.cs
+++ TechnoBackend/TechnoBackend.Tests/Unittest/News.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechnoBackend.Login;
 
 namespace TechnoBackend.Tests.Unittest
 {
@@ -7,9 +11,31 @@ namespace TechnoBackend.Tests.Unittest
     public class News
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NewsCreate()
         {
-            var Testdata = "{'ArticleID':'1','ArticleTitle':'Henk','ArticleBody':'Knabbel','ArticleIMG':'IMG','ArticleLINK':'URL'}";
+            var testdataCreate = "{'ArticleID':'1','ArticleTitle':'Henk','ArticleBody':'Knabbel','ArticleIMG':'IMG','ArticleLINK':'URL'}";
+            string LoginData = "{ 'Username':'Henk','Password':'Knabbel'}";
+
+            var checkreturn = SessionCheck.GetToken("Henk");
+            var token = checkreturn.Item1;
+
+            var body = UTF8Encoding.UTF8.GetBytes(testdataCreate);
+            var testCreateNews = WebRequest.CreateHttp("http://localhost:51516/api/News");
+            testCreateNews.Method = "POST";
+            testCreateNews.Headers.Add("Token",token);
+            var stream = testCreateNews.GetRequestStream();
+            stream.Write(body,0,body.Length);
+            var response = testCreateNews.GetResponse();
+
+            //TODO: Decode response message to get body
+
+
+            var expected = "Article Added succesfully";
+
+            Assert.AreEqual(response,expected);
+
+
+
         }
     }
 }
