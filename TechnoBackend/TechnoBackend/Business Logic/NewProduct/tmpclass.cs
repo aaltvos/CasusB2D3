5949959cb3035a174074a3baeea132be4d6e180c
Kay Simons <kaysimons97@gmail.com>
2017-06-19 12:19:34 +0200

    UC2.0 Voegproduct toe Branch
    branch aangemaakt en controller toegevoegd

---
+++ TechnoBackend/TechnoBackend/Business Logic/NewProduct/tmpclass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechnoBackend.Business_Logic.NewProduct
+{
+    public class tmpclass
+    {
+    }
+}
