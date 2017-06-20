c59748edbab77300712a118da637356a9529a046
Daan <daanm96@gmail.com>
2017-06-20 15:56:36 +0200

    Class added

---
+++ TechnoBackend/TechnoBackend/Business Logic/ProfielBewerken/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http.Controllers;
+using TechnoBackend.DatabaseModel;
+
+namespace TechnoBackend.Business_Logic.ProfielBewerken
+{
+    public class Class1
+    {
+        public void Hall()
+        {
+            
+        }
+        
+        
+    }
+}
