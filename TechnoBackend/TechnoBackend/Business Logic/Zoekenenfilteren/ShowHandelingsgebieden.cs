eaec89b06bf7be5a005e36b85702162c7749b2c5
aaltvos <aalt@aaltvos.nl>
2017-06-22 16:50:31 +0200

    Map in business logic aangemaakt(ZoekenenFilteren) Controller class aangemaakt(Productzoekenenfilteren) In busniness logic twee classes aangemaakt Jsonzoekenenfilteren en Showhandelingsgebieden

---
+++ TechnoBackend/TechnoBackend/Business Logic/Zoekenenfilteren/ShowHandelingsgebieden.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TechnoBackend.DatabaseModel;
+using System.Linq;
+
+namespace TechnoBackend.Business_Logic.Zoekenenfilteren
+{
+    public class ShowHandelingsgebieden
+    {
+        public static IList<HAND_GEB> GetHand_Geb(int numberOfHand_Geb)
+        {
+
+        }
+    }
+}
---
+++ TechnoBackend/TechnoBackend/Controllers/ProductzoekenenfilterenController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TechnoBackend.Business_Logic.Zoekenenfilteren;
+using TechnoBackend.Login;
+
+namespace TechnoBackend.Controllers
+{
+    public class ProductzoekenenfilterenController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 1)
+            {
+                try
+                {
+                    var message = ShowHandelingsgebieden.GetHand_Geb(id);
+                    Request.Headers.Add("Token", newtoken.Item1);
+                    var response = Request.CreateResponse(message);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Session not found");
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
