b576c16417c0b452a176ed488a4b107ad2134e71
Jetrang <1359533lonussen@zuyd.nl>
2017-06-24 21:12:47 +0200

    Code voor Workitem Opvragen 0.1

--- TechnoBackend/TechnoBackend/Business Logic/WorkitemOpvragen/WorkitemOpvragen.cs
+++ TechnoBackend/TechnoBackend/Business Logic/WorkitemOpvragen/WorkitemOpvragen.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TechnoBackend.DatabaseModel;
 
 namespace TechnoBackend.Business_Logic.WorkitemOpvragen
 {
-    public class WorkitemOpvragenController
+    public class WorkitemOpvragen
     {
-        public static IList<PRODs>GetHand_Geb()
+        public static IList<PRODs>GetWorkItem()
         {
+            List<PRODs> prodList = new List<PRODs>();
+            using (DBModelContainer dbModel = new DBModelContainer())
+            {
+                foreach (var x in dbModel.PRODs)
+                {
+                    PRODs y = new PRODs();
+                    y.PRODs_ID = x.Prod_ID;
+                    y.PRODs_Name = x.Prod_Name;
+                    y.PRODs_Dat = x.Prod_Dat.ToString();
 
+                    prodList.Add(y);
+
+                    
+
+                   
+                }
+                return prodList;
+            }
 
         }
     }
--- TechnoBackend/TechnoBackend/Controllers/WorkItemOpvragenController.cs
+++ TechnoBackend/TechnoBackend/Controllers/WorkItemOpvragenController.cs
@@ -3,15 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TechnoBackend.Business_Logic.WorkitemOpvragen;
+using TechnoBackend.Login;
+
 
 namespace TechnoBackend.Controllers
 {
-    public class WorkItemOpvragenController : Controller
+    public class WorkItemOpvragenController : ApiController
     {
-        // GET: WorkItemOpvragen
-        public ActionResult Index()
+
+        public HttpResponseMessage Get()   // allemaal ophalen
         {
-            return View();
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 1)
+            {
+                try
+                {
+                    var message = WorkitemOpvragen.GetWorkItem();             
+                    Request.Headers.Add("Token", newtoken.Item1);
+                    var response = Request.CreateResponse(message);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Session not found");
         }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+
+
+
+
+
+
+
+
     }
 }
