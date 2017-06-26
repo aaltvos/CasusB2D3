d68682fd1db177c51261d2f7891fa450981d6684
Pixelated <folkert.wolfs@gmail.com>
2017-06-26 19:59:36 +0200

    add Event files

---
+++ TechnoBackend/TechnoBackend/Business Logic/Event/CreateEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using Newtonsoft.Json;
+using TechnoBackend.DBmodelContainer;
+using TechnoBackend.Business_Logic.Event;
+
+namespace TechnoBackend.Business_Logic.ManageNews
+{
+    public class CreateEvents
+    {
+        public static string AddEvent(HttpActionContext actionContext)
+        {
+            using (DBmodelContainer data = new DBmodelContainer())
+            {
+                var json = actionContext.Request.Content.ReadAsStringAsync().Result;
+                JsonEvent newEvent = JsonConvert.DeserializeObject<JsonEvent>(json);
+
+                string Token = actionContext.Request.Headers.GetValues("Token").First();
+
+                var UserID = (from sessions in db.SESSIONS where sessions.SESSIONS_Token == Token select sessions.USER_Id.USER_Id).First();
+                var CurrentUserQuery = db.USERs.Where(s => s.USER_Id == UserID);
+                var currentUser = CurrentUserQuery.FirstOrDefault<USERs>();
+
+                EVENT event = new EVENT()
+            }
+        }
+    }
+}
--- TechnoBackend/TechnoBackend/Controllers/EventController.cs
+++ TechnoBackend/TechnoBackend/Controllers/EventController.cs
@@ -4,27 +4,49 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TechnoBackend.Business_Logic.Event;
 using TechnoBackend.Login;
 
 namespace TechnoBackend.Controllers
 {
     public class EventController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/<controller>/5
-        public string Get(int id)
+        public string Get()
         {
-            return "value";
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 1)
+            {
+                try
+                {
+                    var message = ShowEvent.GetEvent();
+                    Request.Headers.Add("Token", newtoken.Item1);
+                    var response = Request.CreateResponse(message);
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Session not found");
         }
 
         // POST api/<controller>
         public void Post([FromBody]string value)
         {
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 2)
+            {
+                var message = CreateEvent.AddEvent(ActionContext);
+
+                Request.Headers.Add("Token", newtoken.Item1);
+                var response = Request.CreateResponse(message);
+                return response;
+            }
+            return Request.CreateResponse("Invalid action");
         }
 
         // PUT api/<controller>/5
