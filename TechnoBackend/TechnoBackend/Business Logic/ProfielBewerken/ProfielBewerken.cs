8f5174b1cd314fec6ec338fb61434a17cc904d30
Daan Martens <1443925martens@zuyd.nl>
2017-06-27 12:24:29 +0200

    v0.5

---
+++ TechnoBackend/TechnoBackend/Business Logic/ProfielBewerken/ProfielBewerken.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http.Controllers;
+using TechnoBackend.Business_Logic.ProfielBewerken;
+
+
+namespace TechnoBackend.Business_Logic.ProfielBewerken
+{
+    public class ProfielBewerken
+    {
+        private static List<Object> getCurrentDetails()
+        {
+
+            List<Object> userDetails = new List<Object>();
+
+            // Geef aan met welke database de linq querry wordt uitgevoerd
+            using (DBModelContainer db = new DBModelContainer())
+            {
+                string Token = waar vandaan ?; 
+                
+
+
+                int USER_Id = (from i in db.SESSIONS where i.SESSIONS_Token = Token select i.SESSIONS.USER_Id)
+
+                // parser om linq querryable om te zetten naar lijst met PRODs
+                foreach (var item in Workitems)
+                {
+                    Lijstje.Add(item);
+                }
+
+                // return Lijst met ongevalideerde workitems
+                return Lijstje;
+            }
+        }
+
+
+
+    }
+}
+
+
+//[USER_Id] int IDENTITY(1,1) NOT NULL,
+
+//[USER_Name] nvarchar(max)  NOT NULL,
+
+//[USER_PW] nvarchar(max)  NOT NULL,
+
+//[USER_Sec] int NOT NULL,
+//    [USER_Val_dat] datetime NOT NULL
--- TechnoBackend/TechnoBackend/Controllers/ProfielBewerkenController.cs
+++ TechnoBackend/TechnoBackend/Controllers/ProfielBewerkenController.cs
@@ -4,21 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TechnoBackend.Login;
+using TechnoBackend.Business_Logic.ProfielBewerken;
 
 namespace TechnoBackend.Controllers
 {
     public class ProfielBewerkenController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
+       
         // GET api/<controller>/5
         public string Get(int id)
         {
-            return "value";
+            var token = ActionContext.Request.Headers.GetValues("Token").First();
+            var newtoken = SessionCheck.Check(token);
+            if (newtoken.Item1 != "no session" && newtoken.Item2 >= 1)
+            {
+                var message = ProfielBewerken.UserDetails();
+                Request.Headers.Add("Token", newtoken.Item1);
+                var response = Request.CreateResponse(message);
+                return response;
+            }
+            return Request.CreateResponse("Invalid action");
+
         }
 
         // POST api/<controller>
