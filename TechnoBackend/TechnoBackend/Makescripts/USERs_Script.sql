b9a71270724dc1d8bc3738fca8084d5aaa074eea
Folkert Wolfs <folkert.wolfs@gmail.com>
2017-06-28 21:30:40 +0200

    Add SQL Makescripts

---
+++ TechnoBackend/TechnoBackend/Makescripts/USERs_Script.sql
@@ -0,0 +1,94 @@
+Delete USERs
+DBCC CHECKIDENT('Ergo.dbo.USERs', RESEED, 0)
+
+--Security level: 4   Beheerder / supervisor
+Insert USERs
+	(
+		Ergo.dbo.USER_Name,
+		USER_PW,
+		USER_Sec,
+		USER_Val_dat,
+		USER_Made_Work
+	)
+	VALUES
+	(
+		'Beheerder',
+		'Beheerder12',
+		4,
+		DATEADD(day,500,CONVERT(date, GETDATE())),
+		'1'
+	)
+
+Insert USERs
+	(
+		Ergo.dbo.USER_Name,
+		USER_PW,
+		USER_Sec,
+		USER_Val_dat,
+		USER_Made_Work
+	)
+	VALUES
+	(
+		'Henk',
+		'Knabbel',
+		4,
+		DATEADD(day,500,CONVERT(date, GETDATE())),
+		'1'
+	)
+
+--Security level: 3    Student
+Insert USERs
+	(
+		Ergo.dbo.USER_Name,
+		USER_PW,
+		USER_Sec,
+		USER_Val_dat,
+		USER_Made_Work
+	)
+	VALUES
+	(
+		'Student',
+		'Student12',
+		3,
+		DATEADD(day,500,CONVERT(date, GETDATE())),
+		'1'
+	)
+
+--Security level: 2   leverancier / ergotherapeuten / externen
+Insert USERs
+	(
+		Ergo.dbo.USER_Name,
+		USER_PW,
+		USER_Sec,
+		USER_Val_dat,
+		USER_Made_Work
+	)
+	VALUES
+	(
+		'Peter',
+		'Henk12',
+		2,
+		DATEADD(day,500,CONVERT(date, GETDATE())),
+		'1'
+	)
+
+--Security level: 1
+Insert USERs
+	(
+		Ergo.dbo.USER_Name,
+		USER_PW,
+		USER_Sec,
+		USER_Val_dat,
+		USER_Made_Work
+	)
+	VALUES
+	(
+		'Sjors',
+		'Sjors12',
+		1,
+		DATEADD(day,500,CONVERT(date, GETDATE())),
+		'1'
+	)
+
+Select * from USERs
+		
