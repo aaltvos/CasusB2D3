ad7806c391d39710782db56513a8f86e5befab44
Joshua Pierlo <1535137pierlo@zuyd.nl>
2017-06-29 16:03:06 +0200

    Opnieuw aangemaakt -  Controllerclass :           ProductZoekenController - Business Logic class :   ProductZoeken

---
+++ TechnoBackend/TechnoBackend/Business Logic/Zoekenenfilteren/ProductZoeken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TechnoBackend.DatabaseModel;
+using System.Linq;
+
+namespace TechnoBackend.Business_Logic.ProductZoeken
+{
+    public class ProductZoeken
+    {
+        public static IList<handGebModel> GetHand_Geb()
+        {
+            List<handGebModel> Productenlijst = new List<handGebModel>();
+            using (DBModelContainer dbModel = new DBModelContainer())
+            {
+                //dbModel.HAND_GEB.Select(x => new lcclsAccDueList { * }).ToList();
+                foreach (var a in dbModel.HAND_GEB)
+                {
+                    handGebModel b = new handGebModel();
+                    b.Prod_ID = a.Prod_ID;
+                    b.Prod_Name = a.Prod_Name;
+                    b.Prod_Cost = a.Prod_Cost;
+
+
+                    handgeblist.Add(b);
+                }
+                return handgeblist;
+            }
+
+
+        }
+    }
+}
---
+++ TechnoBackend/TechnoBackend/Controllers/ProductZoekenController.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TechnoBackend.Login;
+
+namespace TechnoBackend.Controllers
+{
+    public class ProductZoekenController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            if (AuthenticationAttribute.Authent(ActionContext) != "Unauthorized")
+            {
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                response.Headers.Add("Token", AuthenticationAttribute.Authent(ActionContext));
+
+                return response;
+            }
+
+            else { return Request.CreateResponse(HttpStatusCode.Unauthorized); }
+        }
+
+        // GET: api/Authentication/5
+        public string Get(int id)
+        {
+
+            return "value";
+        }
+
+
+        // PUT: api/Authentication/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Authentication/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
